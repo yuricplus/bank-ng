cd5290b12f3ae8f65ac90c2d711f1942bcb6463c
yuricplus <yurigamer16@gmail.com>
2021-07-22 00:53:30 -0300

    Feature: Change app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../shared/services/users.service'
+
+import { UserInterface } from '../shared/interface/user'
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Desafio Picpay Front-end';
+export class AppComponent extends UserService implements OnInit {
+  title = 'Desafio Picpay Front-end || Yuri Martins';
+  public users: Array<UserInterface> = []
+
+  constructor(
+    private _http: HttpClient,
+  ){
+    super(_http)
+  }
+
+  ngOnInit(): void {
+    this.getUsers('/5d531c4f2e0000620081ddce')
+      .subscribe(response => {
+        this.users = response;
+      })
+  }
 }
