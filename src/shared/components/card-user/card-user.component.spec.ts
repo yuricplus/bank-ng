71cf43960259d0360d8e24cbaf7c2c274c74a4e7
yuricplus <yurigamer16@gmail.com>
2021-07-22 23:38:34 -0300

    Feature: Test card component

--- src/shared/components/card-user/card-user.component.spec.ts
+++ src/shared/components/card-user/card-user.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CardUserComponent } from './card-user.component';
+
+const mockUser = {
+  id: 1001,
+  img: "https://randomuser.me/api/portraits/men/9.jpg",
+  name: "Eduardo Santos",
+  username: "@eduardo.santos",
+}
 
 describe('CardUserComponent', () => {
   let component: CardUserComponent;
@@ -8,7 +16,9 @@ describe('CardUserComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CardUserComponent ]
+      imports: [FormsModule],
+      declarations: [ CardUserComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   });
@@ -16,10 +26,31 @@ describe('CardUserComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CardUserComponent);
     component = fixture.componentInstance;
+    component.user = mockUser;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render id and username', () => {
+    const fixture = TestBed.createComponent(CardUserComponent);
+    component = fixture.componentInstance;
+    component.user = mockUser;
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('div.user-id-username')
+      .textContent).toContain('ID: 1001 - Username: @eduardo.santos');
+  });
+
+  it('should render name', () => {
+    const fixture = TestBed.createComponent(CardUserComponent);
+    component = fixture.componentInstance;
+    component.user = mockUser;
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('div.user-name')
+      .textContent).toContain('duardo Santos');
   });
 });
