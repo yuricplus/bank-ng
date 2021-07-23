b488b8fb39fdca9cd994a63e9d8921d9d659e949
yuricplus <yurigamer16@gmail.com>
2021-07-22 23:38:52 -0300

    Feature: Test modal component

--- src/shared/components/modal/modal.component.spec.ts
+++ src/shared/components/modal/modal.component.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgxMaskModule } from 'ngx-mask'
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 import { ModalComponent } from './modal.component';
+import { CardUserComponent } from '../card-user/card-user.component';
+
+const mockUser = {
+  id: 1001,
+  img: "https://randomuser.me/api/portraits/men/9.jpg",
+  name: "Eduardo Santos",
+  username: "@eduardo.santos",
+}
 
 describe('ModalComponent', () => {
   let component: ModalComponent;
@@ -8,7 +18,13 @@ describe('ModalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ModalComponent ]
+      declarations: [ ModalComponent ],
+      imports: [
+        FormsModule,
+        NgxMaskModule.forRoot(),
+        ReactiveFormsModule
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   });
@@ -16,10 +32,48 @@ describe('ModalComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ModalComponent);
     component = fixture.componentInstance;
+    component.modalOpened = true;
+    component.userToPay = mockUser;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render modal info payment success', ()=> {
+    const fixture = TestBed.createComponent(ModalComponent);
+    component = fixture.componentInstance;
+    component.modalOpened = true;
+    component.paymentComplete = true;
+    component.paymentSuccess = true;
+    component.userToPay = mockUser;
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.modal-information .payment-success')
+      .textContent).toContain('O pagamento foi concluido com sucesso');
+  })
+
+  it('should render modal info payment error', ()=> {
+    const fixture = TestBed.createComponent(ModalComponent);
+    component = fixture.componentInstance;
+    component.modalOpened = true;
+    component.paymentComplete = true;
+    component.paymentSuccess = false;
+    component.userToPay = mockUser;
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.modal-information .payment-error')
+      .textContent).toContain('O pagamento não foi concluido com sucesso.');
+  })
+
+  it('should render user to pay', ()=> {
+    const fixture = TestBed.createComponent(ModalComponent);
+    component = fixture.componentInstance;
+    component.modalOpened = true;
+    component.userToPay = mockUser;
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('span.name-user').textContent).toContain('Eduardo Santos');
+  })
 });
